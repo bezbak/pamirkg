35d9a0c424f2278dd92f899ee89388ac035d5dbe
bezbak <Cemetey.2019@gmail.com>
2023-06-08 00:20:26 +0600

    add req

--- pamir/settings.py
+++ pamir/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'pamir.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pamir',
+        'USER': 'pamiradmin',
+        'PASSWORD': 'pamir2023',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
